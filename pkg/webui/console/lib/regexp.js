619c7a5bfe612a05fbb5b0bf2448a88a347f2e66
pgalic96 <p.galic96@gmail.com>
2020-03-13 14:10:12 +0100

    console: Add regex for splitting duration string

--- pkg/webui/console/lib/regexp.js
+++ pkg/webui/console/lib/regexp.js
@@ -24,3 +24,4 @@ export const mqttPassword = /^(?![\s\S])|.{2,100}/
 export const latitude = /^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?)$/
 export const longitude = /^\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)$/
 export const int32 = /^-?\d+$/
+export const unit = new RegExp('[a-z]{1,}')
